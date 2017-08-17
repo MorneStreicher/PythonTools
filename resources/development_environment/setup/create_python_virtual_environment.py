b3e76425085bc52dc2a6ad490f1b5ed38339c9d0
Morne Streicher <MorneStreicher@hotmail.com>
2017-08-17 06:53:48 +0200

    Minor updates

---
+++ resources/development_environment/setup/create_python_virtual_environment.py
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+virtualenv -p /usr/bin/python2.7 morne
+source morne/bin/activate
---
+++ resources/development_environment/setup/install_python_dependancies.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# From: https://gist.github.com/FarhadurFahim/73c0fad6350332cef7a653bcd762f08d
+
+sudo apt-get install python3-pip
+sudo pip3 install virtualenv
