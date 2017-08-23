b177d07a1b146fa06a68acac92d339d4cd0f2559
Morne Streicher <MorneStreicher@hotmail.com>
2017-08-23 19:25:41 +0200

    Updated the InnoDB test to work with JSON types, instead of having a wide table.

--- src/morne/test/mysql/performance/storage_engines/StorageEngineTest.py
+++ src/morne/test/mysql/performance/storage_engines/StorageEngineTest.py
@@ -1,4 +1,5 @@
 import argparse
+import json
 import random
 
 import morne.sdk.app.Log as Log
@@ -6,7 +7,6 @@ from morne.sdk.app.Application import Application, ApplicationConfig
 from morne.sdk.app.Timer import Timer
 from morne.sdk.perfcounters import Counter, PerfCounters
 from morne.sdk.sql.Sql import Sql
-
 
 class StoragEngineTest(Application):
     def __init__(self, _args):
@@ -62,26 +62,23 @@ class StoragEngineTest(Application):
             values["index_5"] = str((random.randint(0, 9999999999))).zfill(10)
 
             # Do the insert
-            sql = "INSERT INTO test_table(varchar_1, varchar_2, varchar_3, varchar_4, varchar_5, varchar_6, varchar_7, varchar_8, varchar_9, varchar_10, varchar_11, varchar_12, varchar_13, varchar_14, varchar_15, varchar_16, varchar_17, varchar_18, varchar_19, varchar_20, " \
-                  "float_1, float_2, float_3, float_4, float_5, float_6, float_7, float_8, float_9, float_10, float_11, float_12, float_13, float_14, float_15, float_16, float_17, float_18, float_19, float_20," \
-                  "index_1, index_2, index_3, index_4, index_5) VALUES " \
-                  "(%(varchar_1)s, %(varchar_2)s, %(varchar_3)s, %(varchar_4)s, %(varchar_5)s, %(varchar_6)s, %(varchar_7)s, %(varchar_8)s, %(varchar_9)s, %(varchar_10)s, %(varchar_11)s, %(varchar_12)s, %(varchar_13)s, %(varchar_14)s, %(varchar_15)s, %(varchar_16)s, %(varchar_17)s, %(varchar_18)s, %(varchar_19)s, %(varchar_20)s, " \
-                  "%(float_1)s, %(float_2)s, %(float_3)s, %(float_4)s, %(float_5)s, %(float_6)s, %(float_7)s, %(float_8)s, %(float_9)s, %(float_10)s, %(float_11)s, %(float_12)s, %(float_13)s, %(float_14)s, %(float_15)s, %(float_16)s, %(float_17)s, %(float_18)s, %(float_19)s, %(float_20)s," \
-                  "%(index_1)s, %(index_2)s, %(index_3)s, %(index_4)s, %(index_5)s)"
-            Sql.execute("test_database", sql, values, self.PERF_SQL_INSERT_DATA)
+            sql = "INSERT INTO test_table(jdoc) VALUES " \
+                  "(%s)"
+            Sql.execute("test_database", sql, json.dumps(values), self.PERF_SQL_INSERT_DATA)
             self.PERF_SQL_TOTAL_ROW_COUNT.apply()
 
             # Do the update, if we need to
             perc_to_update = int(self.application_config().get_application_setting("PercentageToUpdate", "50"))
-            if random.randint(0, 100) <= perc_to_update:
+            if random.randint(1, 100) <= perc_to_update:
                 id_to_update = self.PERF_SQL_TOTAL_ROW_COUNT.get_current_value() - 1000000
-                sql = "UPDATE test_table SET float_1 = float_1 + 1, float_2 = float_2 + 1, float_3 = float_3 + 1 WHERE id = %s" % id_to_update
+                sql = "UPDATE test_table SET jdoc = JSON_SET(jdoc, '$.float_1', JSON_EXTRACT(jdoc, '$.float_1') + 1, '$.float_2', JSON_EXTRACT(jdoc, '$.float_2') + 1, '$.float_3', JSON_EXTRACT(jdoc, '$.float_3') + 1) WHERE id = %s" % id_to_update
                 Sql.execute("test_database", sql, values, self.PERF_SQL_UPDATE_DATA)
 
     def _do_large_query_work(self):
+        Log.Log.log().info("Starting large query worker...")
         while True:
             id = self.PERF_SQL_TOTAL_ROW_COUNT._count
-            sql = "SELECT SUM(float_1) FROM test_table WHERE id >= %s AND id <= %s" % (id-2000000, id)
+            sql = "SELECT MAX(JSON_UNQUOTE(JSON_EXTRACT(jdoc, '$.float_10'))) FROM test_table WHERE id >= %s AND id <= %s" % (id-2000000, id)
             Sql.execute("test_database", sql, None, self.PERF_SQL_LARGE_QUERY)
 
     def _dump_table_status(self):
